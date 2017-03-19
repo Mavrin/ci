35aebf30ae7683f22d8ec224d7efd7b5e09644e5
konstantin krivlenia <krivlenia@gmail.com>
2017-03-19 23:26:35 +0300

    fail test

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -4,4 +4,7 @@ describe('test cool module', () => {
   it('Answer to the Ultimate Question of Life, the Universe, and Everything', () => {
     assert.equal(42, coolModule.doSomething())
   })
+  it('Fail test', () => {
+    assert.equal(40, coolModule.doSomething())
+  })
 })
